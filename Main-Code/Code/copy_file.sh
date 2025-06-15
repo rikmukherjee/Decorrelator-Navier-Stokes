e8fa323e30d43c1a923c3e2bb64ed5f5e0edfe3f
Ritwik Mukherjee <146749425+rikmukherjee@users.noreply.github.com>
2025-06-15 14:45:14 +0530

    Delete Main-Code directory

--- Main-Code/Code/copy_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-\cp part.exe ../Data/Re2/Kappa1/Re_p1/
-\cp part.exe ../Data/Re2/Kappa1/Re_p2/
-\cp part.exe ../Data/Re2/Kappa2/Re_p1/
-\cp part.exe ../Data/Re2/Kappa2/Re_p2/
-\cp part.exe ../Data/Re2/Kappa3/Re_p1/
-\cp part.exe ../Data/Re2/Kappa3/Re_p2/
--- Main-Code/Code/lag_acclaration.f90
+++
@@ -1,9 +0,0 @@
-	subroutine lag_acclaration 
-
-	use mod_serial_fluid
-  implicit none
-
-	Ap = (Vp-Vp0)/delta	
-	Vp0 = Vp
-		
-	end subroutine lag_acclaration 
